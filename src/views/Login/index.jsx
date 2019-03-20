2cf1d34cb6c8f16c7c04beb384cd3fd86280f079
Fernan-Ramos <frs0012@alu.ubu.es>
2019-03-20 19:49:05 +0100

    Installed react-router-dom

---
+++ src/views/Login/index.jsx
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+
+import './style.scss';
+
+
+class LoginView extends PureComponent {
+
+
+    render() {
+
+    }
+}
+
+export default connect(null, mapDispatchToProps)(LoginForm);
