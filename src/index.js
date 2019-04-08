27954cf550f74266cdfe5e700fba4c5ef394583b
Fernan-Ramos <frs0012@alu.ubu.es>
2019-04-08 21:07:01 +0200

    Added resize function

--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './js/views/App';
-import { Provider } from 'react-redux';
-import configureStore from './js/store';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./js/views/App";
+import { Provider } from "react-redux";
+import configureStore from "./js/store";
+import * as serviceWorker from "./serviceWorker";
 
-import 'scss/base/corrections.scss';
-import 'scss/base/reset.scss';
+import "scss/base/corrections.scss";
+import "scss/base/reset.scss";
 
-import 'scss/theme/layout.scss';
-import 'scss/theme/fonts.scss';
-
+import "scss/theme/layout.scss";
+import "scss/theme/fonts.scss";
 
 const store = configureStore();
 
+window.onresize = window.onload = () => {
+  if (window.innerWidth <= 768) {
+    document.documentElement.classList.add("mobile");
+    document.documentElement.classList.remove("desktop");
+  } else {
+    document.documentElement.classList.remove("mobile");
+    document.documentElement.classList.add("desktop");
+  }
+};
+
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
