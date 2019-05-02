a69440e054788e63a397368a60a12a0695ff35a1
Fernan-Ramos <frs0012@alu.ubu.es>
2019-05-02 12:53:31 +0200

    Deleted inecesary files

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Router } from "react-router-dom";
-import "./index.css";
-import App from "./js/views/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
 import routeManager from 'js/services/routeManager';
-import { Provider } from "react-redux";
-import configureStore from "./js/store";
-import * as serviceWorker from "./serviceWorker";
+import { Provider } from 'react-redux';
+import App from './js/views/App';
+import configureStore from './js/store';
+import * as serviceWorker from './serviceWorker';
 
 
-import "scss/base/corrections.scss";
-import "scss/base/reset.scss";
+import 'scss/base/corrections.scss';
+import 'scss/base/reset.scss';
 
-import "scss/theme/layout.scss";
-import "scss/theme/fonts.scss";
+import 'scss/theme/layout.scss';
+import 'scss/theme/fonts.scss';
 
 const store = configureStore();
 
 window.onresize = window.onload = () => {
   if (window.innerWidth <= 768) {
-    document.documentElement.classList.add("mobile");
-    document.documentElement.classList.remove("desktop");
+    document.documentElement.classList.add('mobile');
+    document.documentElement.classList.remove('desktop');
   } else {
-    document.documentElement.classList.remove("mobile");
-    document.documentElement.classList.add("desktop");
+    document.documentElement.classList.remove('mobile');
+    document.documentElement.classList.add('desktop');
   }
 };
 
@@ -33,7 +32,7 @@ ReactDOM.render(
       <App />
     </Router>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
