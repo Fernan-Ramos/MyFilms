f06c8532c23ea48bc40fe61eabb9ea5311ae31ff
Fernan-Ramos <ferburgos93@gmail.com>
2019-10-05 17:07:20 +0200

    Hide API KEY

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 
 
 const config = {
-  apiKey: 'AIzaSyC2fs54th_CGIIoW3yzF2kBCW3Fkz0KECo',
-  authDomain: 'myfilmsreview.firebaseapp.com',
-  databaseURL: 'https://myfilmsreview.firebaseio.com',
-  projectId: 'myfilmsreview',
-  storageBucket: '',
-  messagingSenderId: '720498637533',
-  appId: '1:720498637533:web:8f0ef93b67a8837eb93934',
-  measurementId: 'G-2PCX6LBYXR'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENET_ID
 };
 
 class Firebase {
